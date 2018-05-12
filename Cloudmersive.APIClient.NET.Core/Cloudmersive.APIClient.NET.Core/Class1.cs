235c81a0cda0354a5cf19e3bba2d3241a0ded7ac
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-05-12 14:11:53 -0700

    Update

--- Cloudmersive.APIClient.NET.Core/Cloudmersive.APIClient.NET.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Cloudmersive.APIClient.NET.Core
-{
-    public class Class1
-    {
-    }
-}
---
+++ Cloudmersive.APIClient.NET.Core/Cloudmersive.APIClient.NET.Core/CloudmersiveClientBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloudmersive.APIClient.NET.Core
+{
+    public abstract class CloudmersiveClientBase
+    {
+
+
+        protected string Apikey;
+
+        public long GetApikeyUsage()
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add("Apikey", Apikey);
+                client.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+
+
+
+                var response = client.DownloadString("https://api.cloudmersive.com/nlp/apikeyusage/" + Apikey);
+
+                return System.Convert.ToInt64(response);
+            }
+        }
+
+        public void LoadApikeyFromConfig()
+        {
+            // Try web.config
+
+            System.Configuration.Configuration rootWebConfig1 =
+                System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration(null);
+
+            System.Configuration.KeyValueConfigurationElement customSetting =
+                    rootWebConfig1.AppSettings.Settings["CloudmersiveApikey"];
+
+            if (customSetting != null)
+            {
+                Apikey = customSetting.Value;
+            }
+            else
+            {
+                // No web config, try App.config
+
+                string apikeySettingValue = ConfigurationManager.AppSettings["CloudmersiveApikey"];
+
+                if (apikeySettingValue != null)
+                {
+                    Apikey = apikeySettingValue;
+                }
+            }
+        }
+
+
+
+
+    }
+}
